234bc2c0af473016a41a6cefd3739557a47d4508
elliot! <benjikrolfanpageuwu@gmail.com>
2025-06-19 09:41:53 +0100

    Add files via upload

---
+++ PropWardrobe/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using UnityEngine;
+
+[BepInPlugin("com.elliot.gorillatag.propwardrobe", "PropWardrobe", "1.0.0")]
+public class Plugin : BaseUnityPlugin
+{
+    private GameObject satelliteWardrobe;
+
+    void Update()
+    {
+        if (satelliteWardrobe == null)
+        {
+            var bayouMain = GameObject.Find("BayouMain");
+            if (bayouMain != null)
+            {
+                var computerArea = bayouMain.transform.Find("ComputerArea");
+                if (computerArea != null)
+                {
+                    var wardrobe = computerArea.Find("SatelliteWardrobe");
+                    if (wardrobe != null)
+                    {
+                        satelliteWardrobe = wardrobe.gameObject;
+                    }
+                }
+            }
+        }
+
+        if (satelliteWardrobe != null && !satelliteWardrobe.activeSelf)
+        {
+            satelliteWardrobe.SetActive(true);
+        }
+    }
+}
---
+++ PropWardrobe/PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace PropWardrobe
+{
+    public static class PluginInfo
+    {
+        public const string GUID = "com.elliot.gorillatag.propwardrobe";
+        public const string Name = "PropWardrobe";
+        public const string Version = "1.0.0";
+    }
+}
